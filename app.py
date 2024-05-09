4e5f97b54d80b8ce8f447a6e86e5be1b6c73bdd6
qudsia99 <136015250+qudsia99@users.noreply.github.com>
2024-05-09 12:04:25 -0400

    cleaning up file locations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import pandas as pd
 from rec_engine import get_recs 
